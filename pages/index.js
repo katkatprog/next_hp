fb0ff185d2771afbf50b3ec4ccfc72fc4fa48f8a
katkatprog <katkatprog@gmail.com>
2021-12-30 21:16:36 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title='Home'>
+      {/* <div className={styles.container}> */}
+      
+        <p className='text-4xl'>Welcome to Next.js!</p>
+
+    </Layout>
+      
+  )
+}
